306f0ef2b61942be0a266a125c0ad23adce1b6ba
Khafia Ayyub <85446654+KhafiaAyyub@users.noreply.github.com>
2021-08-21 22:06:25 +0530

    Create 50. Pow(x, n).java

---
+++ LeetCode/Medium/50. Pow(x, n).java
@@ -0,0 +1,19 @@
+/* 50. Pow(x, n)
+Medium
+
+Implement pow(x, n), which calculates x raised to the power n (i.e., xn).
+
+Input: x = 2.00000, n = 10
+Output: 1024.00000
+
+Input: x = 2.00000, n = -2
+Output: 0.25000
+Explanation: 2-2 = 1/22 = 1/4 = 0.25
+ 
+
+Constraints:
+-100.0 < x < 100.0
+-231 <= n <= 231-1
+-104 <= xn <= 104
+
+*/

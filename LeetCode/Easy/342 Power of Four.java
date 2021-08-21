5fb7bd0cdc32c4257f59d4335458ed2364f09dd4
Khafia Ayyub <85446654+KhafiaAyyub@users.noreply.github.com>
2021-08-21 22:04:13 +0530

    Update

--- LeetCode/Easy/342 Power of Four.java
+++ LeetCode/Easy/342 Power of Four.java
@@ -6,6 +6,15 @@ An integer n is a power of four, if there exists an integer x such that n == 4x.
 
 Input: n = 16
 Output: true
-Constraints:
 
+Constraints:
 -231 <= n <= 231 - 1
+
+*/
+
+class Solution {
+    public boolean isPowerOfFour(int num) {
+     return (num> 0) && ((num & (num-1)) == 0) &&(num%3==1);
+        
+    }
+}

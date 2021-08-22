154e7fc2ffef45068a8fcbb53a7cea312879f209
Khafia Ayyub <85446654+KhafiaAyyub@users.noreply.github.com>
2021-08-22 22:40:52 +0530

    Update 1929. Concatenation of Array.java

--- LeetCode/Easy/1929. Concatenation of Array.java
+++ LeetCode/Easy/1929. Concatenation of Array.java
@@ -1,1 +1,16 @@
-/*
+class Solution {
+    public int[] getConcatenation(int[] n) {
+        int arr[] = new int[n.length * 2];
+        
+        for(int i =0;i<n.length;i++){
+            arr[i] = n[i];
+        }
+            int k = 0;
+            for(int i=n.length;i<arr.length;i++){
+                arr[i] = n[k];
+                k++;
+            }
+            return arr;
+        
+    }
+}

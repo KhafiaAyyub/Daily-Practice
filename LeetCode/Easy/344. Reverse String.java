9e2339ff61f318c2035e217f5d40a543b1073581
Khafia Ayyub <85446654+KhafiaAyyub@users.noreply.github.com>
2021-08-23 22:39:00 +0530

    Update

--- LeetCode/Easy/344. Reverse String.java
+++ LeetCode/Easy/344. Reverse String.java
@@ -1,18 +1,4 @@
-/* 344. Reverse String
-Easy
 
-Write a function that reverses a string. The input string is given as an array of characters s.
-
-Example 1:
-
-Input: s = ["h","e","l","l","o"]
-Output: ["o","l","l","e","h"]
-
-Constraints:
-1 <= s.length <= 105
-s[i] is a printable ascii character.
-
-*/
 
 class Solution {
     public void reverseString(char[] s) {
